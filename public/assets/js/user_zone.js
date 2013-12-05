49a2595e1f14ceaf50430d2ce7dddef475730d54
unknown <Urnotalone@Urnotalone.(none)>
2013-12-05 20:57:38 +0800

    Signed-off-by: unknown <Urnotalone@Urnotalone.(none)>

--- public/assets/js/user_zone.js
+++ public/assets/js/user_zone.js
@@ -17,8 +17,15 @@ $(document).ready(function(){
 				key_field_list:list,
 				remark:$('#user-remark').val()
 			},
-			success:function(){
-				window.location = 'quiz.php';
+			success:function(data){
+				if (!isNaN(data))
+				{
+					alert("创建成功");
+					window.location = 'quiz.php?quiz_id=data';
+				}else
+				{
+					alert("创建失败");
+				}
 			}
 		});
 	});
@@ -46,6 +53,27 @@ $(document).ready(function(){
 });
 
 function readpromise(){
+	if(this.id == 'department'){
+		$.ajax({
+			type:'POST',
+			url:'handle/user_zone.php',
+			data:{
+				operation:'CHECKDEPARTMENTQUESTIONNAIRE',
+			},
+			success:function(data){
+				if(data == 1){
+					var returnVal = window.confirm('已经存在未填完的单位问卷，确定要再创建一份单位问卷吗？','是否创建？');
+					if(!returnVal){
+						return;
+					}
+				}
+				else if(data != 0){
+					alert(data);
+				}
+			}
+		});
+		
+	}
 	hide();
 	$('#user-promise').show();
 }
@@ -63,8 +91,74 @@ function user_test(){
 		}
 	});
 }
+function depart_test(){
+	$.ajax({
+		type:'POST',
+		url:'handle/user_zone.php',
+		data:{
+			operation:'CREATEDEPARTMENTQUESTIONNAIRE',
+		},
+		success:function(data){
+			if(!isNaN(data)){
+				alert('创建成功，请到我的测评中的单位测评中进行测评');
+				$('#depart_quiz').click();
+			}
+			else{
+				alert(data);
+			}
+		}
+	});
+}
+function nc_list(){
+	$.ajax({
+		type:'POST',
+		url:'handle/user_zone.php',
+		data:{
+			operation:'FETCHUSERQUESTIONNAIRELIST',
+			state:0
+		},
+		success:function(data){
+			hide();
+			$('#nc-list-items').html(data);
+			$('#nc-list').show();
+		}
+	});
+}
+function c_list(){
+	$.ajax({
+		type:'POST',
+		url:'handle/user_zone.php',
+		data:{
+			operation:'FETCHUSERQUESTIONNAIRELIST',
+			state:1
+		},
+		success:function(data){
+			hide();
+			$('#c-list-items').html(data);
+			$('#c-list').show();
+		}
+	});
+}
+function d_list(){
+	$.ajax({
+		type:'POST',
+		url:'handle/user_zone.php',
+		data:{
+			operation:'FETCHDEPARTMENTQUESTIONNAIRE',
+		},
+		success:function(data){
+			hide();
+			$('#d-list-items').html(data);
+			$('#d-list').show();
+		}
+	});
+}
 
 function hide(){
+	$('#nc-list').hide();
+	$('#c-list').hide();
+	$('#d-list').hide();
 	$('#user-promise').hide();
 	$('#select-quiz').hide();
+	$('#change-passwd').hide();
 }
--- public/user_zone.php
+++ public/user_zone.php
@@ -26,62 +26,146 @@ include_once '../sys/core/init.inc.php';
 		<div class="panel-body">
 			<div class="col-md-3">
 				<div class="list-group">
-					<a class="list-group-item active">创建问卷</a>
-					<a href="javascript:readpromise()" class="list-group-item text-center" >个人测评</a>
-					<a href="#" class="list-group-item text-center">单位测评</a>
+					<a class="list-group-item active">创建测评</a>
+					<a href="javascript:readpromise()" id="person" class="list-group-item text-center" >创建个人测评</a>
+					<a href="javascript:depart_test()" id="department" class="list-group-item text-center">创建单位测评</a>
 					<a class="list-group-item active">我的测评</a>
-					<a href="#" class="list-group-item text-center">未完成</a>
-					<a href="#" class="list-group-item text-center">已完成</a>
-					<a href="#" class="list-group-item text-center">单位评测</a>
+					<a href="javascript:nc_list()" class="list-group-item text-center">未完成的测评</a>
+					<a href="javascript:c_list()" class="list-group-item text-center">已完成的测评</a>
+					<a href="javascript:d_list()" id="depart_quiz" class="list-group-item text-center">单位测评</a>
 					<a class="list-group-item active">个人信息修改</a>
 					<a href="#" class="list-group-item text-center">修改密码</a>
 					<a href="#" class="list-group-item text-center">修改资料</a>
 				</div>
 			</div>
 			<div class="col-md-9">
+				
+				<div class="panel panel-default" id="nc-list" style="display:none">
+				  <div class="panel-heading">
+					<h3 class="panel-title">未完成的测评</h3>
+				  </div>
+				  <div class="panel-body">
+				  
+					<div class="list-group" id="nc-list-items">
+					
+					</div>
+				  </div>
+				</div>
+				
+				<div class="panel panel-default" id="c-list" style="display:none">
+				  <div class="panel-heading">
+					<h3 class="panel-title">已完成的测评</h3>
+				  </div>
+				  <div class="panel-body">
+				  
+					<div class="list-group" id="c-list-items">
+						
+					</div>
+				  </div>
+				</div>
+				
+				<div class="panel panel-default" id="d-list" style="display:none">
+				  <div class="panel-heading">
+					<h3 class="panel-title">单位测评</h3>
+				  </div>
+				  <div class="panel-body">
+				  
+					<div class="list-group" id="d-list-items">
+					  <
+					</div>
+				  </div>
+				</div>
 			
-				<div class="row" id="user-promise" style="display:none;">
+				<div class="panel panel-default" id="user-promise" style="display:none;">
+				  <div class="panel-heading">
+					<h3 class="panel-title">请仔细阅读承诺书</h3>
+				  </div>
+				  <div class="panel-body">
+				  
 					<div class="row">
-						<div class="col-md-1"></div>
-						<div class="col-md-10">
-							<div class="panel panel-default">
-							  <div class="panel-heading">承诺书</div>
-							  <div class="panel-body">
-								Some default panel content here. Nulla vitae elit libero, a pharetra augue. Aenean lacinia bibendum nulla sed consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id dolor id nibh ultricies vehicula ut id elit.
-							  </div>
+						<div class="row">
+							<div class="col-md-1"></div>
+							<div class="col-md-10">
+								<div class="panel panel-default">
+								  <div class="panel-heading">承诺书</div>
+								  <div class="panel-body">
+									Some default panel content here. Nulla vitae elit libero, a pharetra augue. Aenean lacinia bibendum nulla sed consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id dolor id nibh ultricies vehicula ut id elit.
+								  </div>
+								</div>
 							</div>
+							<div class="col-md-1"></div>
 						</div>
-						<div class="col-md-1"></div>
-					</div>
-					<div class="row">
-						<div class="col-md-12 text-center">
-							<button class="btn btn-success" id="readit">我已经仔细阅读《XXX》并同意</button>
-						</div>
-					</div>
-				</div>
-				
-				<div class="row" id="select-quiz" style="display:block;">
-					<div class="row">
-						<div class="col-md-12">
-							<div class="list-group" id="field-select">
-								<input type="checkbox" id="box1" value="1">
-								<input type="checkbox" id="box12" value="2">
+						<div class="row">
+							<div class="col-md-12 text-center">
+								<button class="btn btn-success" id="readit">我已经仔细阅读《XXX》并同意相关内容</button>
 							</div>
 						</div>
 					</div>
-					<div class="row">
-						<div class="col-md-12">
-							<label class="label-control">输入备注（可选）：</label>
-							<textarea id="user-remark" class="form-control" rows="3"></textarea>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-12 text-center">
-							<button class="btn btn-success" id="create">确认创建</button>
-						</div>
-					</div>
+				  </div>
 				</div>
 				
+				<div class="panel panel-default" id="select-quiz" style="display:none;">
+				  <div class="panel-heading">
+					<h3 class="panel-title">选择你想要测评的域然后创建</h3>
+				  </div>
+				  <div class="panel-body">
+				  
+					<div class="row" >
+						<div class="row">
+							<div class="col-md-12">
+								<div class="list-group" id="field-select">
+									
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							
+							<div class="col-md-12">
+								<label class="label-control">输入备注（可选）：</label>
+								<textarea id="user-remark" class="form-control" rows="3"></textarea>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12 text-center">
+								<button class="btn btn-success" id="create">确认创建</button>
+							</div>
+						</div>
+					</div>
+				  </div>
+				</div>
+				
+				<div class="panel panel-default" id="change-passwd" style="display:none">
+				  <div class="panel-heading">
+					<h3 class="panel-title">选择你想要测评的域然后创建</h3>
+				  </div>
+				  <div class="panel-body">
+					<div class="form-horizontal">
+						<div class="form-group">
+							<label class="control-label col-md-4 text-right">原密码：</label>
+							<div class="col-md-4">
+								<input class="form-control" type="password" id="originPasswd" placeholder="输入原密码"/>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="control-label col-md-4 text-right">新密码：</label>
+							<div class="col-md-4">
+								<input class="form-control" type="password" id="newPasswd" placeholder="输入新密码"/>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="control-label col-md-4 text-right">确认新密码：</label>
+							<div class="col-md-4">
+								<input class="form-control" type="password" id="confirmPasswd" placeholder="确认新密码"/>
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="col-md-12 text-center">
+								<button class="btn btn-success" id="newPasswd">确认</button>
+							</div>
+						</div>
+					</div>
+				  </div>
+				</div>
 				
 				
 			</div>
