a11533158401ea460a84b0da79f88b87f56aa531
Demonsu <245681117@qq.com>
2013-12-05 09:39:08 +0800

    Signed-off-by: Demonsu <245681117@qq.com>

---
+++ sys/class/class.Admin.inc.php
@@ -0,0 +1,11 @@
+﻿<?php
+
+class Admin extends DB_Connect {
+
+
+	public function __construct(){
+		parent::__construct();
+	}
+}
+
+?>
---
+++ sys/class/class.User.inc.php
@@ -0,0 +1,11 @@
+﻿<?php
+
+class User extends DB_Connect {
+
+
+	public function __construct(){
+		parent::__construct();
+	}
+}
+
+?>
