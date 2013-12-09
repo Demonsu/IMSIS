bb6bf3f3f7cfb34d42e6f5dd8fbc4b5bc9bf7b00
unknown <Urnotalone@Urnotalone.(none)>
2013-12-09 11:13:11 +0800

    Signed-off-by: unknown <Urnotalone@Urnotalone.(none)>

--- public/include/header.php
+++ public/include/header.php
@@ -9,7 +9,7 @@
 			<h2 class="text-center"  onclick="window.location='user_zone.php';"><strong>电子政务服务能力成熟度在线评估系统<strong><br><small>eGov-CMM</small></h2>
 			<?php
 				if (isset($_SESSION['USERID']))
-					echo '<h4 class="text-right" >'.$_SESSION['USERID'].' <span class="label label-danger">退出</span></h4>';
+					echo '<h4 class="text-right" >'.$_SESSION['USERID'].' <span class="label label-danger" onclick="window.location='.'\''.'login.php'.'\''.'">退出</span></h4>';
 				else
 					echo '<h4 class="text-right"> <span class="label label-success" onclick="window.location='.'\''.'login.php'.'\''.'">登录</span></h4>';
 					
