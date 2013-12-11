929bb91eb7a69781f440f3f51cead66d42be3b44
Demonsu <245681117@qq.com>
2013-12-11 22:07:54 +0800

    kengdied xuqiu

--- sys/class/class.Statistics.inc.php
+++ sys/class/class.Statistics.inc.php
@@ -4,14 +4,15 @@ class KeyVariable
 {
 	public $id;
 	public $question;
-	public $score;
+	public $score=-1;
 	public $key_field_id;
 }
 class KeyField
 {
 	public $id;
 	public $name;
-	public $score;
+	public $score=-1;
+	public $round_score=-1;//尾化处理过的分数
 	public $key_variable_list=array();
 	public $effect_field_id;
 }
@@ -19,7 +20,7 @@ class EffectField
 {
 	public $id;
 	public $name;
-	public $score;
+	public $score=-1;
 	public $key_field_list=array();
 }
 class Quiz extends DB_Connect
@@ -56,7 +57,7 @@ class Quiz extends DB_Connect
 			$sql="SELECT * FROM key_field WHERE available='1' AND effect_field_id='".$effect_field->id."'";
 			$key_field_select=mysql_query($sql,$this->root_conn)or trigger_error(mysql_error(),E_USER_ERROR);	
 			$key_field_num=0;
-			$key_field_total_score=0;	
+			$key_field_total_score=0;
 			while($key_field_info=mysql_fetch_assoc($key_field_select))		
 			{
 				
@@ -91,6 +92,14 @@ class Quiz extends DB_Connect
 				if ($key_variable_num!=0)
 				{
 					$key_field->score=$key_variable_total_score/$key_variable_num;
+					$score=$key_field->score-floor($key_field->score);
+					if ($score>=0 && $score<0.3)
+						$score=0;
+					else if ($score>=0.3 && $score<0.8)
+						$score=0.5;
+					else if ($socre>=0.8 && $score<=1)
+						$score=1;
+					$key_field->round_score=floor($key_field->score)+$score;					
 					$key_field_num++;
 					$key_field_total_score=$key_field_total_score+$key_field->score;
 				}
@@ -194,7 +203,7 @@ class Statistics extends DB_Connect {
 		$jsondata= sprintf($RETURNFORMAT,$all_effect_field);
 		$dir='../statistics/'.$quiz_id;
 		$result=create_folders($dir); 
-		$handle = fopen($dir.'/table1.json', "w ");
+		$handle = fopen($dir.'/table1.json', "w");
 		fwrite($handle,$jsondata);
 		fclose($handle);
 		
@@ -236,10 +245,186 @@ class Statistics extends DB_Connect {
 		
 		$dir='../statistics/'.$quiz_id;
 		$flag=create_folders($dir); 
-		$handle = fopen($dir.'/table2.json', "w ");
+		$handle = fopen($dir.'/table2.json', "w");
 		fwrite($handle,$result);
 		fclose($handle);
 		
+	}
+	public function table3_KDs($quiz_id)
+	{
+		$quiz=new Quiz();
+		$quiz->init_quiz($quiz_id);
+		$KEYFIELDFORMAT='
+		{
+			"title":"%s",
+			"content":"%s"
+		}';	
+		$EFFECTFIELDFORMAT='
+		{
+			"title":"%s",
+			"content":[
+				%s
+			]
+		}';
+		$QUIZFORMAT='
+		{
+		"content":[
+			%s
+		]
+		}';
+		$all_effect_field="";
+		foreach($quiz->effect_field_list as $effect_field)
+		{
+			$all_key_field="";
+			foreach($effect_field->key_field_list as $key_field)
+			{
+				if ($key_field->score!=-1)
+				{
+					if ($all_key_field!="")
+						$all_key_field=$all_key_field.",";
+					$all_key_field=$all_key_field.sprintf($KEYFIELDFORMAT,$key_field->name,$key_field->round_score);
+				}
+			}
+			if ($effect_field->score!=-1)
+			{
+				if ($all_effect_field!="")
+					$all_effect_field=$all_effect_field.",";
+				$all_effect_field=$all_effect_field.sprintf($EFFECTFIELDFORMAT,$effect_field->name,$all_key_field);
+			}
+		}
+		$result=sprintf($QUIZFORMAT,$all_effect_field);
+		$dir='../statistics/'.$quiz_id;
+		$flag=create_folders($dir); 
+		$handle = fopen($dir.'/table3.json', "w");
+		fwrite($handle,$result);
+		fclose($handle);		
+	}
+	public function table4_KDs($quiz_id)
+	{
+		$quiz=new Quiz();
+		$quiz->init_quiz($quiz_id);
+		$RESULTFORMAT='
+			{
+				"content":[
+					{
+						"title":"成熟度特征",
+						"content":["0","0.5","1","1.5","2","2.5","3","3.5","4","4.5","5"]
+					},
+					{
+						"title":"发生项数",
+						"content":["%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s"]
+					},
+					{
+						"title":"百分比",
+						"content":["%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s"]
+					}
+				],
+				"total":["%s","100"]
+			}
+		';
+		$mature_level=0;
+		$total_num=0;
+		$num=array();
+		while ($mature_level<=5)
+		{
+			$key_field_num=0;
+			foreach($quiz->effect_field_list as $effect_field)
+			{
+				foreach($effect_field->key_field_list as $key_field)
+				{
+					if ($key_field->round_score!=-1 && $key_field->round_score==$mature_level)
+					{
+						$key_field_num++;
+					}			
+				}
+			}
+			$num[]=$key_field_num;
+			$mature_level=$mature_level+0.5;
+		}
+		foreach($quiz->effect_field_list as $effect_field)
+		{
+			foreach($effect_field->key_field_list as $key_field)
+			{
+				if ($key_field->round_score!=-1)
+				{
+					$total_num++;
+				}			
+			}
+		}
+		
+		$result=sprintf($RESULTFORMAT,$num[0],$num[1],$num[2],$num[3],$num[4],$num[5],$num[6],$num[7],$num[8],$num[9],$num[10],$num[11],
+									  $num[0]/$total_num*100,$num[1]/$total_num*100,$num[2]/$total_num*100,$num[3]/$total_num*100,$num[4]/$total_num*100,
+									  $num[5]/$total_num*100,$num[6]/$total_num*100,$num[7]/$total_num*100,$num[8]/$total_num*100,$num[9]/$total_num*100,
+									  $num[10]/$total_num*100,$num[11]/$total_num*100,$total_num);	
+		$dir='../statistics/'.$quiz_id;
+		$flag=create_folders($dir); 
+		$handle = fopen($dir.'/table4.json', "w");
+		fwrite($handle,$result);
+		fclose($handle);		
+	}
+	public function table5_LDs($quiz_id)
+	{
+		/*
+		{
+			"content":[
+				{
+					"title":"业务管理",
+					"score":"2.8",
+					"proportion":"28"
+				},
+				{
+					"title":"信息技术",
+					"score":"2.5",
+					"proportion":"25"
+				},
+				{
+					"title":"组织与运营",
+					"score":"2.4",
+					"proportion":"24"
+				},
+				{
+					"title":"电子政务规划",
+					"score":"2.3",
+					"proportion":"23"
+				}
+			]
+		}
+		*/
+		$quiz=new Quiz();
+		$quiz->init_quiz($quiz_id);
+		$EFFECTFIELDFORMAT='
+				{
+					"title":"%s",
+					"score":"%s",
+					"proportion":"%.2f%%"
+				}';
+		$RESULTFORMAT='
+				{
+					"content":[
+						%s
+				]}';
+		$all_effect_field="";
+		$total_score=0;
+		foreach($quiz->effect_field_list as $effect_field)
+		{
+			if ($effect_field!=-1)
+				$total_score+=$effect_field->score;
+		}
+		foreach($quiz->effect_field_list as $effect_field)
+		{
+			if ($all_effect_field!="")
+				$all_effect_field=$all_effect_field.",";
+			if ($effect_field->score!=-1)
+				$all_effect_field=$all_effect_field.sprintf($EFFECTFIELDFORMAT,$effect_field->name,$effect_field->score,$effect_field->score/$total_score*100);
+			else
+				$all_effect_field=$all_effect_field.sprintf($EFFECTFIELDFORMAT,$effect_field->name,0,0.00);
+		}
+		$result=sprintf($RESULTFORMAT,$all_effect_field);
+		$dir='../statistics/'.$quiz_id;
+		$flag=create_folders($dir); 
+		$handle = fopen($dir.'/table5.json', "w");
+		fwrite($handle,$result);
+		fclose($handle);	
 	}
 }
 
