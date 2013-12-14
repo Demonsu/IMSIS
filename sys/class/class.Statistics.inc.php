84992fb1a5a0aa9262e99f4c801ded7f8a43845c
Demonsu <245681117@qq.com>
2013-12-14 14:03:01 +0800

    bug fixed

--- sys/class/class.Statistics.inc.php
+++ sys/class/class.Statistics.inc.php
@@ -852,8 +852,8 @@ class Statistics extends DB_Connect {
 					{
 						if ($key_variable->promote_space>0)
 						{
-							if ($all_variable_field!="")
-								$all_variable_field=$all_variable_field.",";
+							if ($all_variable_list_field!="")
+								$all_variable_list_field=$all_variable_list_field.",";
 							$all_variable_list_field=$all_variable_list_field.sprintf($KEYVARIABLELISTFORMAT,$key_variable->question,$key_variable->score,$key_variable->promote_space);
 						}
 					}
