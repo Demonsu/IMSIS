8a291065e2d2f855c0059869157ee51bcf613260
Ghostcandywyj <ghost.wyj@gmail.com>
2013-12-16 08:46:37 +0800

    wyj

--- public/admin_zone.php
+++ public/admin_zone.php
@@ -67,7 +67,7 @@
 		  <div class="panel-heading" id="body_title">管理作用域</div>
 		  <div class="panel-body">
 			<ul class="list-group" id="effect-field-list">
-			  <li class="list-group-item" id="">Cras justo odiodwads
+			  <li class="list-group-item" id="12">Cras justo odiodwads
 				<label class="label label-danger" onclick="delete_effect_field(this)">删除</label>
 				<label class="label label-info" onclick="show_hide_effect_field(this)">显示</label>
 				<label class="label label-warning" onclick="modify_effect_field(this)">修改</label>
@@ -82,7 +82,8 @@
 			<div class="alert alert-warning">
 				<div class="input-group">
 				  <span class="input-group-addon" id="effect-field-title"></span>
-				  <input type="text" id="effect-field-input" class="form-control" placeholder="作用域名称">
+				  <input type="text" id="effect-field-input" class="form-control" placeholder="作用域名称" >
+				  <input type="text" id="effect-field-id" style="display:none;" value=""/>
 				  <span class="input-group-btn">
 					<button class="btn btn-default" type="button" id="confirm-effect-field">确认</button>
 					<button class="btn btn-default" type="button" id="cancel-effect-field">取消</button>
@@ -96,10 +97,33 @@
 		<div class="panel panel-default">
 		  <div class="panel-heading" id="body_title">管理关键域</div>
 		  <div class="panel-body">
-			<ul class="list-group">
-			
+			<select class="form-control" id="fetch-effect-field-list">
+				
+			</select>
+			<ul class="list-group" id="key-field-list">
+			  <li class="list-group-item" id="">Cras justo odiodwads
+				<label class="label label-danger" onclick="delete_key_field(this)">删除</label>
+				<label class="label label-info" onclick="show_hide_key_field(this)">显示</label>
+				<label class="label label-warning" onclick="modify_key_field(this)">修改</label>
+			  </li>
+			  <li class="list-group-item text-center" onclick="add_key_field()"><span class="glyphicon glyphicon-plus"></span></li>
 			</ul>
 		  </div>
+		</div>
+	</div>
+	<div id="key-field-cover">
+		<div class="key-field-edit">
+			<div class="alert alert-warning">
+				<div class="input-group">
+				  <span class="input-group-addon" id="key-field-title"></span>
+				  <input type="text" id="key-field-input" class="form-control" placeholder="作用域名称">
+				  <input type="text" id="key-field-id" style="display:none;" value=""/>
+				  <span class="input-group-btn">
+					<button class="btn btn-default" type="button" id="confirm-key-field">确认</button>
+					<button class="btn btn-default" type="button" id="cancel-key-field">取消</button>
+				  </span>
+				</div>
+			</div>
 		</div>
 	</div>
 	
@@ -107,10 +131,65 @@
 		<div class="panel panel-default">
 		  <div class="panel-heading" id="body_title">管理关键变量</div>
 		  <div class="panel-body">
-			<ul>
-			
+			<div class="row">
+				<div class="col-md-6">
+					<select class="form-control" id="fetch-effect-field-list2">
+						
+					</select>
+				</div>
+				<div class="col-md-6">
+					<select class="form-control" id="fetch-key-field-list2">
+						
+					</select>
+				</div>
+			</div>
+			<ul class="list-group" id="key-variable-list">
+			  <li class="list-group-item" id="">Cras justo odiodwads
+				<label class="label label-danger" onclick="delete_key_variable(this)">删除</label>
+				<label class="label label-info" onclick="show_hide_key_variable(this)">显示</label>
+				<label class="label label-warning" onclick="modify_key_variable(this)">修改</label>
+			  </li>
+			  <li class="list-group-item text-center" onclick="add_key_variable()"><span class="glyphicon glyphicon-plus"></span></li>
 			</ul>
 		  </div>
+		</div>
+	</div>
+	<div id="key-variable-cover">
+		<div class="key-variable-edit">
+			<div class="alert alert-warning">
+				<div class="input-group" style="float:right">
+
+				  <input type="text" id="key-variable-id" style="display:none;" value=""/>
+				  <div class="btn-group">
+					<button type="button" class="btn btn-default" id="confirm-key-variable">确认</button>
+					<button type="button" class="btn btn-default" id="cancel-key-variable">取消</button>
+				  </div>
+				</div>
+				<div class="input-group">
+				  <span class="input-group-addon">问题</span>
+				  <input type="text" id="key_variable_question" class="form-control" placeholder="问题">
+				</div>
+				<div class="input-group">
+				  <span class="input-group-addon">1分选项</span>
+				  <input type="text" id="key_variable_a" class="form-control" placeholder="1">
+				</div>
+				<div class="input-group">
+				  <span class="input-group-addon">2分选项</span>
+				  <input type="text" id="key_variable_b" class="form-control" placeholder="2">
+				</div>
+				<div class="input-group">
+				  <span class="input-group-addon">3分选项</span>
+				  <input type="text" id="key_variable_c" class="form-control" placeholder="3">
+				</div>
+				<div class="input-group">
+				  <span class="input-group-addon">4分选项</span>
+				  <input type="text" id="key_variable_d" class="form-control" placeholder="4">
+				</div>
+				<div class="input-group">
+				  <span class="input-group-addon">5分选项</span>
+				  <input type="text" id="key_variable_e" class="form-control" placeholder="5">
+				</div>
+			</div>
 		</div>
 	</div>
 	
--- public/assets/js/admin_zone.js
+++ public/assets/js/admin_zone.js
@@ -1,8 +1,12 @@
 var effect_field_change = true;
 var key_field_change = true;
 var key_variable_change = true;
+
+var effect_field1;
+var key_field2;
 $(document).ready(function(){
 	$('#manage-effect-field').click(function(){
+		hide();
 		$.ajax({
 			type:'POST',
 			url:'handle/admin_zone.php',
@@ -13,18 +17,57 @@ $(document).ready(function(){
 				$('#effect-field-list').html(data);
 			}
 		});
-		hide();
 		$('#change-effect-field').show();
 	});
 	$('#manage-key-field').click(function(){
 		hide();
+		$.ajax({
+			type:'POST',
+			url:'handle/admin_zone.php',
+			data:{
+				operation:'FETCHEFFECTFIELDSELECTLIST'
+			},
+			success:function(data){
+				$('#fetch-effect-field-list').html('<option value=0>请选择作用域</option>'+data);
+			}
+		});
 		$('#change-key-field').show();
+	});
+	$('#fetch-effect-field-list').change(function(){
+		effect_field1 = $('#fetch-effect-field-list').val()
+		fetch_key_field_list(effect_field1);
 	});
 	$('#manage-key-variable').click(function(){
 		hide();
+		$.ajax({
+			type:'POST',
+			url:'handle/admin_zone.php',
+			data:{
+				operation:'FETCHEFFECTFIELDSELECTLIST'
+			},
+			success:function(data){
+				$('#fetch-effect-field-list2').html('<option value=0>请选择作用域</option>'+data);
+			}
+		});
 		$('#change-key-variable').show();
 	});
-	
+	$('#fetch-effect-field-list2').change(function(){
+		$.ajax({
+			type:'POST',
+			url:'handle/admin_zone.php',
+			data:{
+				operation:'FETCHKEYFIELDSELECTLIST'
+			},
+			success:function(data){
+				$('#fetch-key-field-list2').html('<option value=0>请选择关键域</option>'+data);
+			}
+		});
+	});
+	$('#fetch-key-field-list2').change(function(){
+		key_field2 = $('#fetch-key-field-list2').val()
+		fetch_key_field_list(key_field2);
+	});
+	//作用域
 	$('#confirm-effect-field').click(function(){
 		if(effect_field_change){
 			$.ajax({
@@ -33,7 +76,7 @@ $(document).ready(function(){
 				data:{
 					operation:'MODIFYEFFECTFIELD',
 					add:0,
-					effect_field_id:t.parentNode.id,
+					effect_field_id:$('#effect-field-id').val(),
 					name:$('#effect-field-input').val()
 				},
 				success:function(data){
@@ -50,7 +93,7 @@ $(document).ready(function(){
 				data:{
 					operation:'MODIFYEFFECTFIELD',
 					add:1,
-					effect_field_id:t.parentNode.id,
+					effect_field_id:'',
 					name:$('#effect-field-input').val()
 				},
 				success:function(data){
@@ -65,8 +108,106 @@ $(document).ready(function(){
 		$('#effect-field-input').val('');
 		$('#effect-field-cover').hide();
 	});
+	//关键域
+	$('#confirm-key-field').click(function(){
+		if(effect_field_change){
+			$.ajax({
+				type:'POST',
+				url:'handle/admin_zone.php',
+				data:{
+					operation:'MODIFYKEYFIELD',
+					add:0,
+					key_field_id:$('#effect-field-id').val(),
+					name:$('#key-field-input').val()
+				},
+				success:function(data){
+					if(data == 1){
+						fetch_key_field_list(effect_field1);
+					}
+				}
+			});
+		}
+		else{
+			$.ajax({
+				type:'POST',
+				url:'handle/admin_zone.php',
+				data:{
+					operation:'MODIFYKEYFIELD',
+					add:1,
+					key_field_id:'',
+					name:$('#key-field-input').val()
+				},
+				success:function(data){
+					if(data == 1){
+						fetch_key_field_list(effect_field1);
+					}
+				}
+			});
+		}
+	});
+	$('#cancel-key-field').click(function(){
+		$('#key-field-input').val('');
+		$('#key-field-cover').hide();
+	});
+	//关键域
+	$('#confirm-key-variable').click(function(){
+		if(effect_field_change){
+			$.ajax({
+				type:'POST',
+				url:'handle/admin_zone.php',
+				data:{
+					operation:'MODIFYKEYVARIABLE',
+					add:0,
+					key_variable_id:$('#effect-variable-id').val(),
+					name:$('#key-variable-input').val()
+				},
+				success:function(data){
+					if(data == 1){
+						fetch_key_variable_list(key_field2);
+					}
+				}
+			});
+		}
+		else{
+			$.ajax({
+				type:'POST',
+				url:'handle/admin_zone.php',
+				data:{
+					operation:'MODIFYKEYVARIABLE',
+					add:1,
+					key_variable_id:'',
+					name:$('#key-variable-input').val()
+				},
+				success:function(data){
+					if(data == 1){
+						fetch_key_variable_list(key_field2);
+					}
+				}
+			});
+		}
+	});
+	$('#cancel-key-variable').click(function(){
+		$('#key-variable-input').val('');
+		$('#key-variable-cover').hide();
+	});
 });
+function fetch_key_field_list(t){
+	$.ajax({
+		type:'POST',
+		url:'handle/admin_zone.php',
+		data:{
+			operation:'FETCHKEYFIELDLIST',
+			effect_field_id:t
+		},
+		success:function(data){
+			$('#key-field-list').html(data);
+		}
+	});
+}
+function fetch_key_variable_list(t){
 
+}
+//这里是作用域的操作
 function delete_effect_field(t){
 	alert('警告！此操作不可逆！');
 	alert('删除后你将再也看不到与该关键域相关的东西');
@@ -93,6 +234,7 @@ function modify_effect_field(t){
 	//alert($(t.parentNode).text().substr(0,$(t.parentNode).text().length-22));
 	effect_field_change = true;
 	$('#effect-field-title').text('修改作用域名称');
+	$('#effect-field-id').val(t.parentNode.id);
 	$('#effect-field-input').val($(t.parentNode).text().substr(0,$(t.parentNode).text().length-27));
 	$('#effect-field-cover').show();
 }
@@ -114,6 +256,150 @@ function show_hide_effect_field(t){
 		url:'handle/admin_zone.php',
 		data:{
 			operation:'SHOWORHIDEEFFECTFIELD',
+			available:temp
+		},
+		success:function(data){
+			if(data == 1){
+				if(temp == 0)
+					$(t).text('隐藏');
+				else
+					$(t).text('显示');
+			}
+			else{
+				alert(data);
+			}
+		}
+	});
+}
+//这里是关键域的操作
+function delete_key_field(t){
+	alert('警告！此操作不可逆！');
+	alert('删除后你将再也看不到与该关键域相关的东西');
+	var returnType = window.confirm('确认删除吗？')
+	if(returnType){
+		$.ajax({
+			type:'POST',
+			url:'handle/admin_zone',
+			data:{
+				operation:'DELETEKEYFIELD',
+				key_field_id:t.parentNode.id
+			},
+			success:function(data){
+				if(data == 1){
+					alert('删除成功');
+				}
+				else
+					alert(data);
+			}
+		});
+	} 
+}
+function modify_key_field(t){
+	//alert($(t.parentNode).text().substr(0,$(t.parentNode).text().length-22));
+	key_field_change = true;
+	$('#key-field-title').text('修改关键域名称');
+	$('#key-field-id').val(t.parentNode.id);
+	$('#key-field-input').val($(t.parentNode).text().substr(0,$(t.parentNode).text().length-27));
+	$('#key-field-cover').show();
+}
+function add_key_field(){
+	key_field_change = false;
+	$('#key-field-title').text('新的关键域名称');
+	$('#key-field-cover').show();
+}
+function show_hide_key_field(t){
+	var temp;
+	if($(t).text() == '显示'){
+		temp = 0;
+	}
+	else
+		temp = 1;
+	
+	$.ajax({
+		type:'POST',
+		url:'handle/admin_zone.php',
+		data:{
+			operation:'SHOWORHIDEKEYFIELD',
+			available:temp
+		},
+		success:function(data){
+			if(data == 1){
+				if(temp == 0)
+					$(t).text('隐藏');
+				else
+					$(t).text('显示');
+			}
+			else{
+				alert(data);
+			}
+		}
+	});
+}
+//关键变量
+function delete_key_variable(t){
+	alert('警告！此操作不可逆！');
+	alert('删除后你将再也看不到与该关键域相关的东西');
+	var returnType = window.confirm('确认删除吗？')
+	if(returnType){
+		$.ajax({
+			type:'POST',
+			url:'handle/admin_zone',
+			data:{
+				operation:'DELETEKEYVARIABLE',
+				key_variable_id:t.parentNode.id
+			},
+			success:function(data){
+				if(data == 1){
+					alert('删除成功');
+				}
+				else
+					alert(data);
+			}
+		});
+	} 
+}
+function modify_key_variable(t){
+	//alert($(t.parentNode).text().substr(0,$(t.parentNode).text().length-22));
+	key_variable_change = true;
+	$('#key-variable-title').text('修改关键域名称');
+	$('#key-variable-id').val(t.parentNode.id);
+	$.ajax({
+		type:'POST',
+		url:'handle/admin_zone.php',
+		data:{
+			operation:'FETCHKEYVARIABLEDETAIL',
+			key_variable_id:t.parentNode.id
+		},
+		success:function(str){
+			var data = jQuery.parseJSON(str);
+			$('#key_variable_question').val(data.question);
+			$('#key_variable_a').val(data.answer_a);
+			$('#key_variable_b').val(data.answer_b);
+			$('#key_variable_c').val(data.answer_c);
+			$('#key_variable_d').val(data.answer_d);
+			$('#key_variable_e').val(data.answer_e);
+		}
+	});
+	$('#key-variable-cover').show();
+}
+function add_key_variable(){
+	key_variable_change = false;
+	$('#key-variable-title').text('新的关键域名称');
+	$('#key-variable-cover').show();
+}
+function show_hide_key_variable(t){
+	var temp;
+	if($(t).text() == '显示'){
+		temp = 0;
+	}
+	else
+		temp = 1;
+	
+	$.ajax({
+		type:'POST',
+		url:'handle/admin_zone.php',
+		data:{
+			operation:'SHOWORHIDEKEYVARIABLE',
 			available:temp
 		},
 		success:function(data){
