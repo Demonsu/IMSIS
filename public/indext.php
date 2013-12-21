6eb31883d0ffbe38ab2c9a57c2a27985ddd14c5b
Ghostcandywyj <ghost.wyj@gmail.com>
2013-12-21 12:56:49 +0800

    index

---
+++ public/indext.php
@@ -0,0 +1,164 @@
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8">
+	<title>电子政务与数据智能Lab-主页</title>
+	
+	<link rel="stylesheet" href="./assets/dist/css/bootstrap.min.css">
+	<script style="text/javascript" src="./assets/js/jquery.js"></script>
+	<script style="text/javascript" src="./assets/dist/js/bootstrap.min.js"></script>
+	<script style="text/javascript" src="./assets/js/index.js"></script>
+	<link rel="stylesheet" href="./assets/css/index.css">
+	<link rel="stylesheet" href="./assets/css/body.css">
+</head>
+<body>
+
+<div class="main">
+	<div class="header-group">
+		<img src="./assets/img/index/logo.png" height="60px" alt="logo" />
+		<div class="navi-container">
+			<nav class="navbar navbar-mine" role="navigation">
+			  <!-- Collect the nav links, forms, and other content for toggling -->
+			  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+				  <li class="active"><a href="#">主页</a></li>
+				  <li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">研究领域<b class="caret"></b></a>
+					<ul class="dropdown-menu">
+					  <li><a href="#">探索视点</a></li>
+					  <li><a href="#">科研成果</a></li>
+					</ul>
+				  </li>
+				  <li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">服务<b class="caret"></b></a>
+					<ul class="dropdown-menu">
+					  <li><a href="#">咨询服务</a></li>
+					  <li><a href="#">开发服务</a></li>
+					  <li><a href="#">运维服务</a></li>
+					</ul>
+				  </li>
+				  <li><a>成员</a></li>
+				  <li><a>其他栏目</a></li>
+				</ul>
+			  </div><!-- /.navbar-collapse -->
+			</nav>
+		</div>
+	</div>
+	
+	<div class="main-wapper">
+		<div class="view-content">
+			<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+			  <!-- Wrapper for slides -->
+			  <div class="carousel-inner">
+				<div class="item active">
+				  <img src="./assets/img/index/stage-healthdata.jpg" alt="...">
+				  <div class="carousel-caption">
+				  </div>
+				</div>
+				<div class="item">
+				  <img src="./assets/img/index/homepage_0.jpg" alt="...">
+				  <div class="carousel-caption">
+				  </div>
+				</div>
+				<div class="item">
+				  <img src="./assets/img/index/homepage.jpg" alt="...">
+				  <div class="carousel-caption">
+				  </div>
+				</div>
+			  </div>
+
+			  <!-- Controls -->
+			  <div style="display:none">
+				  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+					<span class="glyphicon glyphicon-chevron-left"></span>
+				  </a>
+				  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+					<span class="glyphicon glyphicon-chevron-right"></span>
+				  </a>
+			  </div>
+			</div>
+		</div>
+		<div class="brief-content">
+			<div style="width:350px;font-size:20px;text-align:center;color:#ffffff;font-weight:bold;">
+				实验室简介
+			</div>
+			<p style="color:#ffffff;font-size:12px;margin-top:10px">
+				南京大学电子政务与数据智能实验室（E-government & Data Intelligence Laboratory，EG&DI lab）成立于2012年，依托于南京大学信息管理学院，面向电子政务及数据智能，结合当前信息社会发展的需要，开展电子政务系统以及大数据的相关管理与应用研究。主要研究方向包括：电子政务系统的开发与管理、电子政务服务能力管理、大数据智能、互联网商务模式（IBMs）管理、分布式信息系统设计与开发方法等。<br>
+				实验室具备充满活力的科研梯队以及良好的硬件设施环境。胡广伟副教授为实验室主要负责人，固定成员包括硕士研究生8人，本科生多名。实验室先后承担各类科研项目几十项，发表国内外学术期刊论文数十篇，出版专著多部，研究报告多部。实验室本着“充实、完善、凝炼、提升”思想，坚持“开放与联合”的原则，不断开拓进取，努力成为特色鲜明、可持续创新的电子政务系统及大数据智能研究开发基地和人才培养基地。
+			</p>
+		</div>
+	</div>
+	
+	<div class="main-content">
+		<div class="content-container">
+			<div class="content-one">
+				<div style="text-align:center;color:#931414;font-size:20px;font-weight:bold">
+					探索与分享
+				</div>
+				<div style="border:1px solid #aaaaaa;padding:3px;margin-top:10px;">
+					<a href="./assets/download/The_big_data_revolution_in_healthcare.pdf" target="_blank" title="The_big_data_revolution_in_healthcare">
+						<img src="./assets/img/index/pdf-download.jpg" width="277px"/>
+					</a>
+				</div>
+				<ul class="list-group" style="margin-left:50px;" >
+					<li class="list-group-item list-group-item-success">
+						<img src="./assets/img/index/pdf-logo.png" width="36px" />
+						<a title="Hadoop平台架构" href="./assets/download/The_big_data_revolution_in_healthcare.pdf" target="_blank">医疗大数据革命</a></li>
+					<li class="list-group-item list-group-item-success">
+						<img src="./assets/img/index/pptx-logo.png" width="36px" />
+						<a title="Hadoop平台架构" href="./assets/download/Hadoop平台架构.pptx" target="_blank">Hadoop平台架构</a></li>
+					<li class="list-group-item list-group-item-success">
+						<img src="./assets/img/index/zip-logo.png" width="36px" />
+						<a title="电子政务服务能力" href="./assets/download/电子政务服务能力.zip" target="_blank">电子政务服务能力</a></li>
+				</ul>
+			</div>
+			<div class="content-two">
+				<div style="text-align:center;color:#931414;font-size:20px;font-weight:bold">
+					最新动态
+				</div>
+				
+			</div>
+			<div class="content-three">
+				<a href="./login.php">
+					<img style="border: 0; vertical-align: middle;" src="./assets/img/index/eGov-CMM.png" alt="eGoc-CMM" border="0" height="150" width="285" />
+				</a>
+				<div style="text-align:center;color:#931414;font-size:20px;font-weight:bold">
+					友情链接
+				</div>
+				<div class="link-border">
+					<a title="南京大学主页" href="http://nju.edu.cn" target="_blank"><img src="./assets/img/index/link_1.jpg" height="72px"/></a>
+				</div>
+				<div class="link-border">
+					<a title="南京大学信息管理学院"href="http://im.nju.edu.cn/" target="_blank"><img src="./assets/img/index/link_2.jpg" height="72px"/></a>
+				</div>
+				<div class="link-border" style="margin-right:0px;">
+					<a title="中国政务网站服务能力建设网" href="http://www.gwd.gov.cn/" target="_blank"><img src="./assets/img/index/link_3.jpg" height="72px"/></a>
+				</div>
+				<div class="link-border">
+					<a title="北京市政务数据资源网" href="http://www.bjdata.gov.cn/" target="_blank"><img src="./assets/img/index/link_4.jpg" height="72px"/></a>
+				</div>
+				<div class="link-border">
+					<a title="上海市政务数据服务网" href="http://www.datashanghai.gov.cn/" target="_blank"><img src="./assets/img/index/link_5.jpg" height="72px"/></a>
+				</div>
+				<div class="link-border" style="margin-right:0px;">
+					<a title="美国政府数据中心" href="http://www.data.gov/" target="_blank"><img src="./assets/img/index/link_6.jpg" height="72px"/></a>
+				</div>
+				<!--
+				<ul class="list-group" style="text-align:center" >
+				<li class="list-group-item list-group-item-success"><a title="南京大学主页" href="http://nju.edu.cn" target="_blank">南京大学主页</a></li>
+				<li class="list-group-item list-group-item-success"><a title="南京大学信息管理学院" href="http://im.nju.edu.cn/" target="_blank">南京大学信息管理学院</a></li>
+				<li class="list-group-item list-group-item-success"><a title="中国政务网站服务能力建设网" href="http://www.gwd.gov.cn/" target="_blank">中国政务网站服务能力建设网</a>  </li>
+				<li class="list-group-item list-group-item-success"><a title="北京市政务数据资源网" href="http://www.bjdata.gov.cn/" target="_blank">北京市政务数据资源网</a> </li>
+				<li class="list-group-item list-group-item-success"><a title="上海市政务数据服务网" href="http://www.datashanghai.gov.cn/" target="_blank">上海市政务数据服务网</a>  </li>
+				<li class="list-group-item list-group-item-success"><a title="美国政府数据中心" href="http://www.data.gov/" target="_blank">美国政府数据中心</a>  </li>
+				</ul>-->
+			</div>
+		</div>
+	</div>
+	
+	<div class="footer-wapper">
+		<span>Copyright©2013 All Rights Reserved</span>
+	</div>
+</div>
+
+</body>
+</html>
