92dd5a9fa49444a3f59f6b395f5a2b2cd9af476a
Ghostcandywyj <ghost.wyj@gmail.com>
2013-12-30 19:47:55 +0800

    display

--- public/include/index_footer.php
+++ public/include/index_footer.php
@@ -1,7 +1,25 @@
 <div class="footer-container">
-		
-	<span class="footer-cover">
-	<a>联系我们</a> | <a>法律声明</a> | <a>友情链接</a> | <a>网站地图</a>
-	</span><br>
-	<span style="line-height:36px;">Copyright©2013 All Rights Reserved</span>
-</div>
+		<table style="width:100%;">
+			<tr>
+				<td rowspan=2 style="width:350px;text-align:right">
+					<a href="http://www.nju.edu.cn/" target="_blank">
+					<img src="../assets/img/index/nju.png" width="50" />
+					</a>
+				</td>
+				<td>
+					<span class="footer-cover">
+					<a>联系我们</a>
+					| <a>法律声明</a>
+					| <a href="?navigation=7">友情链接</a>
+					| <a>网站地图</a>
+					</span>
+				</td>
+				<td rowspan=2 style="width:350px"></td>
+			</tr>
+			<tr>
+				<td>
+				<span style="line-height:36px;">Copyright©2013 All Rights Reserved</span>
+				</td>
+			</tr>
+		</table>
+	</div>
