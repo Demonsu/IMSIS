eb90a0331330a15af3acce322ac69d976bc96f61
Demonsu <245681117@qq.com>
2014-01-15 13:27:54 +0800

    xf

--- public/handle/index.php
+++ public/handle/index.php
@@ -3,7 +3,9 @@
 	include_once '../../sys/core/init.inc.php';
 	//$system=new System();
 	//echo $system->add();
-	$quiz_id=87;
-	$statistics=new Statistics();
-	$statistics->table_all($quiz_id);
+	//$quiz_id=87;
+	//$statistics=new Statistics();
+	//$statistics->table_all($quiz_id);
+	$system=new System();
+	echo $system->search_website("医疗");
 ?>
