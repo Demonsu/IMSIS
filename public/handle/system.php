86fd353db45bc3f0451d48ea2507c0684f929d5e
Demonsu <245681117@qq.com>
2014-01-23 15:59:24 +0800

    xf

--- public/handle/system.php
+++ public/handle/system.php
@@ -56,5 +56,41 @@
 		$key_word=$_POST["key_word"];
 		echo $system->search_website($key_word);
 	}
+	if ($operation=="ANSWERGOVQUIZ")//回答政府问卷
+	{
+		//answer_list:"1:2;2:3;3:5….."
+		//question_suggestion:"string"(htmlspecialchars) 对应45题
+		//quiz_suggestion: "string"(htmlspecialchars)    对应46题
+		$answer_list=$_POST["answer_list"];
+		$question_suggestion=$_POST["question_suggestion"];
+		$quiz_suggestion=$_POST["quiz_suggestion"];
+		echo $syetem->answer_gov_quiz(explode(';',$answer_list),$question_suggestion,$quiz_suggestion);
+	}
+	if ($operation=="ANSWERPUBQUIZ")//回答公众问卷
+	{
+		$answer_list=$_POST["answer_list"];
+		$quiz_suggestion=$_POST["quiz_suggestion"];		
+		echo $syetem->answer_pub_quiz(explode(';',$answer_list),$quiz_suggestion);
+	}
+	if ($operation=="FETCHGOVQUIZSTATISTICS")//获取政府问卷统计数据
+	{
+		echo $system->fetch_gov_quiz_statistic();
+	}
+	if ($operation=="FETCHGOVQUIZQUESTIONSUGGESTIONLIST")//获取政府问卷问题建议
+	{
+		echo $system->fetch_gov_quiz_question_suggestion();
+	}
+	if ($operation=="FETCHGOVQUIZSUGGESTION")//获取政府问卷建议
+	{
+		echo $system->fetch_gov_quiz_suggestion();
+	}
+	if ($operation=="FETCHPUBQUIZSTATISTICS")//获取公众问卷统计数据
+	{
+		echo $system->fetch_pub_quiz_statistics();
+	}
+	if ($operation=="FETCHPUBQUIZSUGGESTION")//获取公众问卷建议
+	{
+		echo $system->fetch_pub_quiz_suggestion();
+	}
 
 ?>
