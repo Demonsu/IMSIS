33cfee357fd750c4321c9656622d8744a9df4ea7
Demonsu <245681117@qq.com>
2014-03-10 16:20:48 +0800

    xf

--- public/login.php
+++ public/login.php
@@ -1,5 +1,4 @@
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <?php
 	include_once '../sys/core/init.inc.php';
 
